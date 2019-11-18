f70df96dee9784d7ec7df83bb6c65ba3310b0efb
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-11-18 20:06:17 +0000

    Nonce Finder clean

--- noncefinder.py
+++ noncefinder.py
@@ -2,7 +2,6 @@ import hashlib
 import time
 
 class NonceFinder(object):
-
   def find(self, data, difficulty):
     golden_nonce = None
     nonce = 0
@@ -22,3 +21,14 @@ class NonceFinder(object):
   def __digest_hash(self, data, nonce):
     block = (data + str(nonce)).encode("utf-8")
     return hashlib.sha256(block).hexdigest()
+
+if __name__ == "__main__":
+  data = "COMSM0010cloud"
+  difficulty = 5
+  start_time = time.time()
+  golden_nonce, hash = NonceFinder().find(data, difficulty)
+  processing_time = time.time() - start_time
+  print("Difficulty: ", difficulty)
+  print("Processing time: {0:.4f} s.".format(processing_time))
+  print("Golden Nonce is", golden_nonce)
+  print("Hash: ", hash)
