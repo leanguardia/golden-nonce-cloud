04adead1a54ec95aaf7b9457886fea91014da7db
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-11-19 14:43:10 +0000

    Async Finder working

--- async_finder.py
+++ async_finder.py
@@ -1,8 +1,9 @@
+from collections import deque
+import hashlib
+import time
 from celery import Celery
 from celery.result import ResultSet
 from celery.task.control import inspect
-import hashlib
-import time
 
 broker_url = 'amqp://guest:guest@localhost:5672'
 app = Celery('async_finder', broker=broker_url, backend='rpc://')
@@ -27,12 +28,57 @@ def find(data, difficulty, batch, start, end):
       break
   return None
 
+def sched_task(data, difficulty, index, scale=10000):
+  start = index * scale
+  end = start + scale
+  return find.delay(data, difficulty, batch, start, end)
+
+def nonce_found(results):
+  for result in results:
+    if result.status == 'SUCCESS':
+      if result.result != None:
+        return result.result
+  return False
+
+def values(results):
+  values = []
+  for result in results:
+    if result.status == 'SUCCESS':
+      values.append(result.result)
+    else:
+      values.append('N/A')
+  return values
+
+def get_nonce(results):
+  for result in results:
+    if result.status == 'SUCCESS' and result.result != None:
+      return result.result
+  return "Not Found"
+
+
 if __name__ == '__main__':
   data = "COMSM0010cloud"
   difficulty = 5
-  N = 2
+  N = 3
   golden_nonce = None
   scale = 10000
   batch = 0
+  results = deque()
   
-  results = ResultSet([])
+  for i in range(N + 1):
+    results.append(sched_task(data, difficulty, batch, scale))
+    batch += 1
+
+  while(not nonce_found(results)):
+    finished = 0
+    for result in results:
+      if result.status == 'SUCCESS':
+        finished += 1; break
+    for i in range(finished):
+      results.popleft()
+      results.append(sched_task(data, difficulty, batch, scale))
+      batch += 1
+
+    print(values(results))
+
+  print("Golden Nonce: ", get_nonce(results))
