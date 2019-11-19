ede28d5bb9e25ca878641fe2dbf0af5c77dd4eca
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-11-19 01:53:20 +0000

    Clean Async Engine

--- async_finder.py
+++ async_finder.py
@@ -1,4 +1,6 @@
-from celery import Celery, result
+from celery import Celery
+from celery.result import ResultSet
+from celery.task.control import inspect
 import hashlib
 import time
 
@@ -8,7 +10,6 @@ app = Celery('async_finder', broker=broker_url, backend='rpc://')
 def valid_hash(hash, difficulty):
     for i in range(difficulty):
       if hash[i] != '0': return False 
-    print("Golden Nonce Found!")
     return True
 
 def digest_hash(data, nonce):
@@ -16,32 +17,22 @@ def digest_hash(data, nonce):
   return hashlib.sha256(block).hexdigest()
 
 @app.task
-def find(data, difficulty, start, end):
-  print(f"Testing from: {start} to {end}")
+def find(data, difficulty, batch, start, end):
+  print(f"Batch {batch}: {start} to {end}")
   for nonce in range(start, end):
     hash = digest_hash(data, nonce)
     if valid_hash(hash, difficulty):
       print(f"Golden Nonce Found!: {nonce}")
       return nonce
       break
-  print("Nothing found")
   return None
 
 if __name__ == '__main__':
   data = "COMSM0010cloud"
-  difficulty = 6
-  index = 0
-  machines = 75
-  results = result.ResultSet([])
+  difficulty = 5
+  N = 2
   golden_nonce = None
-  # while golden_nonce == None:
-  for machine in range(machines):
-    scale = 100000
-    start = index * scale
-    end = (index * scale) + scale
-    ranch = range(start, end)
-    async_result = find.delay(data, difficulty, start, end)
-    results.add(async_result)
-    index = index + 1
-
-  print("results", results.join())
+  scale = 10000
+  batch = 0
+  
+  results = ResultSet([])
