9159d0881a27e2c0d490db0810761e6fcd3a6d40
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-11-21 01:46:05 +0000

    AsyncFinder Add Pre processing logs

--- app/async_finder.py
+++ app/async_finder.py
@@ -6,7 +6,7 @@ from celery.result import ResultSet
 from celery.task.control import inspect
 
 broker_url = 'amqp://guest:guest@localhost:5672'
-app = Celery('async_finder', broker=broker_url, backend='rpc://')
+app = Celery('app.async_finder', broker=broker_url, backend='rpc://')
 
 def valid_hash(hash, difficulty):
     for i in range(difficulty):
@@ -28,9 +28,9 @@ def find(data, difficulty, batch, start, end):
       break
   return None
 
-def sched_task(data, difficulty, index, scale=10000):
-  start = index * scale
-  end = start + scale
+def sched_task(data, difficulty, index, batch_size=10000):
+  start = index * batch_size
+  end = start + batch_size
   return find.delay(data, difficulty, batch, start, end)
 
 def values(results):
@@ -51,16 +51,20 @@ def get_nonce(results):
 if __name__ == '__main__':
   data = "COMSM0010cloud"
   difficulty = 8
-  N = 1
+  num_of_workers = 4
+  batch_size = 1000000
+
+  print(f"Searching a Golden Nonce for {data} on {time.ctime()}")
+  print("Difficulty:", difficulty)
+  print("N of workers:", num_of_workers)
+  print("Batch size:", batch_size)
+  start_time = time.time()
+
   golden_nonce = None
-  scale = 10000000
   batch = 0
   results = deque()
-
-  start_time = time.time()
-  
-  for i in range(N + 20):
-    results.append(sched_task(data, difficulty, batch, scale))
+  for i in range(num_of_workers * 7):
+    results.append(sched_task(data, difficulty, batch, batch_size))
     batch += 1
 
   while(not golden_nonce):
@@ -71,11 +75,11 @@ if __name__ == '__main__':
     golden_nonce = get_nonce(results)
     for i in range(finished):
       results.popleft()
-      results.append(sched_task(data, difficulty, batch, scale))
+      results.append(sched_task(data, difficulty, batch, batch_size))
       batch += 1
 
   processing_time = time.time() - start_time
   print("Difficulty: ", difficulty)
-  print("N of machines: ", N)
+  print("N of workers:", num_of_workers)
   print("Processing time: {0:.4f} s.".format(processing_time))
   print("Golden Nonce is", golden_nonce)
