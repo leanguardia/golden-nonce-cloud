9644e4eff59898d76d2ff66979ad748f9e2ee1a5
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-12-07 18:06:51 +0000

    SQS Next Task

--- sqs.py
+++ sqs.py
@@ -31,31 +31,42 @@ class Sqs(object):
     MessageGroupId=("UniqueID")
   )
 
-  # def next_task(self):
-  #   response = sqs.receive_message(
-  #     QueueUrl=self.tasks_queue_url,
-  #     AttributeNames=[
-  #         'SentTimestamp'
-  #     ],
-  #     MaxNumberOfMessages=1,
-  #     MessageAttributeNames=[
-  #         'All'
-  #     ],
-  #     VisibilityTimeout=0,
-  #     WaitTimeSeconds=0
-  #   )
-  #   message = response['Messages'][0]
-  #   receipt_handle = message['ReceiptHandle']
+  def next_task(self):
+    response = self.client.receive_message(
+      QueueUrl=self.tasks_queue_url,
+      AttributeNames=['All'],
+      MaxNumberOfMessages=1,
+      MessageAttributeNames=['All'],
+      WaitTimeSeconds=1
+    )
+    if not 'Messages' in response: print("-> NO MESSAGES!!!")
+    return self.__build_task(response['Messages'][0])
 
   # def complete_task(self, message_id):
   #   sqs.delete_message(
   #       QueueUrl=tasks_queue_url,
   #       ReceiptHandle=receipt_handle
   #   )
+
+  # def delete_task(receipt_handle):
+  #   self.client()
+
   def purge_all(self):
-    print("Purging Queues")
+    print("--> Purging Queues")
     self.client.purge_queue(QueueUrl=self.tasks_queue_url)
 
+  def __build_task(self, message):
+    attributes = message['MessageAttributes']
+    # print(message['MessageAttributes'])
+    print(message['MessageId'])
+    return {
+      'Body': message['Body'],
+      'SearchFrom': attributes['SearchFrom']['StringValue'],
+      'SearchTo': attributes['SearchTo']['StringValue'],
+      'Difficulty': attributes['Difficulty']['StringValue'],
+      'Data': attributes['Data']['StringValue'],
+      'MessageID': message['MessageId'],
+    }
 
 if __name__ == "__main__":
   data = "CLOUDSMSlalala"
@@ -65,10 +76,21 @@ if __name__ == "__main__":
   batch_size = 100
   num_of_tasks = 10
 
-  for task_index in range(num_of_tasks):
-    search_from = task_index * batch_size
-    search_to = search_from + batch_size - 1
-    sqs.create_task(data, difficulty, (search_from, search_to))
+  # for task_index in range(num_of_tasks):
+  #   search_from = task_index * batch_size
+  #   search_to = search_from + batch_size - 1
+  #   sqs.create_task(data, difficulty, (search_from, search_to))
+
+  task = sqs.next_task()
+  print(task)
+
+  print("Body", task["Body"])
+  print("SearchFrom", task["SearchFrom"])
+  print("SearchTo", task["SearchTo"])
+  print("Difficulty", task["Difficulty"])
+  print("Data", task["Data"])
+  print("MessageId", task["MessageID"])
+  # sps.delete_task(task)
 
   # sqs.stop_search()
   # sqs.complete_task()
