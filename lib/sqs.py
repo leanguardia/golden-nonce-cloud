80a3cfe437bf5e4e972f66f0a0c354109aec3822
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-12-08 02:56:37 +0000

    Sqs Include Hexdigest in Stop message

--- lib/sqs.py
+++ lib/sqs.py
@@ -6,7 +6,7 @@ class Sqs(object):
     self.stop_search_queue_url  = 'https://sqs.us-east-1.amazonaws.com/398055134224/stop-search.fifo'
     self.client = boto3.client('sqs')
 
-  def create_task(self, data, difficulty, search_range):
+  def send_task(self, data, difficulty, search_range):
     search_from, search_to = search_range
     response = self.client.send_message(
     QueueUrl=self.tasks_queue_url,
@@ -32,11 +32,11 @@ class Sqs(object):
     MessageGroupId=("UniqueID")
   )
 
-  def next_task(self, max_retries=9):
+  def poll_task(self, max_retries=9):
     message = None
     attempt_num = 1
     while(not message and attempt_num <= max_retries):
-      if attempt_num > 1: print("Receive Message Attempt:", attempt_num)
+      if attempt_num > 1: print("Receive Message Attempt (Poll):", attempt_num)
       response = self.client.receive_message(
         QueueUrl=self.tasks_queue_url,
         AttributeNames=['All'],
@@ -46,24 +46,29 @@ class Sqs(object):
       )
       if 'Messages' in response: message = response['Messages'][0]
       attempt_num += 1
-    return self.__build_task(response['Messages'][0]) if message else False
+    return self.__parse_task(response['Messages'][0]) if message else False
 
-  def complete_task(self, task):
+  def delete_message(self, receive_handle):
     response = self.client.delete_message(
       QueueUrl=self.tasks_queue_url,
-      ReceiptHandle=task['ReceiptHandle']
+      ReceiptHandle=receive_handle
     )
 
   def purge_all(self):
-    print("--> Purging Queues")
+    self.purge_tasks_queue()
+    self.purge_stop_queue()
+
+  def purge_tasks_queue(self):
     self.client.purge_queue(QueueUrl=self.tasks_queue_url)
+
+  def purge_stop_queue(self):
     self.client.purge_queue(QueueUrl=self.stop_search_queue_url)
 
   def stop_search(self, max_retries=3):
     message = None
     attempt_num = 1
     while(not message and attempt_num <= max_retries):
-      if attempt_num > 1: print("Receive Message Attempt:", attempt_num)
+      if attempt_num > 1: print("Receive Message Attempt (Stop):", attempt_num)
       response = self.client.receive_message(
         QueueUrl=self.stop_search_queue_url,
         AttributeNames=['All'],
@@ -73,34 +78,53 @@ class Sqs(object):
       )
       if 'Messages' in response: message = response['Messages'][0]
       attempt_num += 1
-    return message
+    return self.__parse_stop_message(response['Messages'][0]) if message else False
 
-  def publish_golden_nonce(self, nonce, binary_sequence):
+  def publish_golden_nonce(self, nonce, binary_sequence, hexdigest):
     response = self.client.send_message(
       QueueUrl=self.stop_search_queue_url,
       MessageBody=(f"Golden {nonce}: {binary_sequence}|({len(binary_sequence)})"),
       MessageAttributes={
+          'StopReason': {
+            'DataType': 'String',
+            'StringValue': "NonceFound"
+          },
           'Nonce': {
-              'DataType': 'Number',
-              'StringValue': str(nonce)
+            'DataType': 'Number',
+            'StringValue': str(nonce)
           },
           'BinarySequence': {
-              'DataType': 'String',
-              'StringValue': str(binary_sequence)
+            'DataType': 'String',
+            'StringValue': str(binary_sequence)
+          },
+          'Hexdigest': {
+            'DataType': 'String',
+            'StringValue': hexdigest
           },
       },
       MessageGroupId=("UniqueID")
     )
 
-  def __build_task(self, message):
+  def __parse_task(self, message):
     attributes = message['MessageAttributes']
     return {
       'Body': message['Body'],
+      'ReceiptHandle': message['ReceiptHandle'],
       'SearchFrom': int(attributes['SearchFrom']['StringValue']),
       'SearchTo': int(attributes['SearchTo']['StringValue']),
       'Difficulty': int(attributes['Difficulty']['StringValue']),
       'Data': attributes['Data']['StringValue'],
+    }
+
+  def __parse_stop_message(self, message):
+    attributes = message['MessageAttributes']
+    return {
+      'Body': message['Body'],
       'ReceiptHandle': message['ReceiptHandle'],
+      'StopReason': attributes['StopReason']['StringValue'],
+      'Nonce': int(attributes['Nonce']['StringValue']),
+      'BinarySequence': attributes['BinarySequence']['StringValue'],
+      'Hexdigest': attributes['Hexdigest']['StringValue'],
     }
 
 if __name__ == "__main__":
@@ -108,7 +132,7 @@ if __name__ == "__main__":
   # difficulty = 3
   sqs = Sqs()
 
-  # batch_size = 100
+  # batch_size = 10000
   # num_of_tasks = 100
 
   # for task_index in range(num_of_tasks):
