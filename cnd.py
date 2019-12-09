b877c119f803f97d3bfc32eef0bea30de9081cda
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-12-09 20:24:50 +0000

    Cnd balanced send tasks

--- cnd.py
+++ cnd.py
@@ -1,6 +1,6 @@
 import time
 from worker import Worker
-from lib.sqs import Sqs
+from lib.sqs import sqs.Sqs
 from app.nonce_evaluator import NonceEvaluator
 
 def unwrap_golden(message):
@@ -12,15 +12,16 @@ def unwrap_golden(message):
   )
 
 def send_tasks_batch(sqs, task_index, batch_size, num_of_tasks, difficulty, data):
+  start_time = time.time()
   batch_number = int(task_index / num_of_tasks) + 1
   starting_at = task_index * batch_size
   tasks_per_batch = num_of_tasks * batch_size
   ending_at = batch_number * tasks_per_batch - 1
-  print(f"Batch {batch_number} | {num_of_tasks} tasks of size {batch_size} from {starting_at} to {ending_at}")
   for index in range(task_index, task_index + num_of_tasks):
     search_from = index * batch_size
     search_to = search_from + batch_size - 1
-    # sqs.send_task(data, difficulty, (search_from, search_to))
+    sqs.send_task(data, difficulty, (search_from, search_to))
+  print(f"Batch {batch_number} | {num_of_tasks} tasks of size {batch_size} from {starting_at} to {ending_at} in {time.time() - start_time} seconds.")
   return task_index + num_of_tasks
 
 if __name__ == "__main__":
@@ -28,19 +29,19 @@ if __name__ == "__main__":
   # print("Data:", data, "| Difficulty:", difficulty)
 
   data = 'COMSM0010cloud'
-  difficulty = 5
+  difficulty = 7
 
   # SETUP QUEUES
   sqs = Sqs()
-  # sqs.purge_all()  CREATE
+  # sqs.purge_all() # CREATE
   # sqs.purge_tasks_queue()
   
 
   # CREATE FIRST BATCH
   task_index = 0
-  num_of_tasks = 100
+  num_of_tasks = 15
   batch_size = 10000
-  threshold = 50
+  threshold = 11
 
   task_index = send_tasks_batch(sqs, task_index, batch_size, num_of_tasks, difficulty, data)
 
@@ -48,7 +49,8 @@ if __name__ == "__main__":
   searching = True
 
   while searching:
-    tasks_in_queue = sqs.approx_num_of_tasks()
+    print("About to request num of tasks")
+    tasks_in_queue = sqs.approx_num_of_tasks() # do this every now and then
     print("Tasks in queue:", tasks_in_queue)
     if tasks_in_queue < threshold:
       task_index = send_tasks_batch(sqs, task_index, batch_size, num_of_tasks, difficulty, data)
