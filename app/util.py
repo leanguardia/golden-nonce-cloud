665218c768e54c3f6ae24a7c42a08be4b1da6514
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-12-11 14:46:18 +0000

    Include N in arguments parser

--- app/util.py
+++ app/util.py
@@ -1,14 +1,22 @@
 import sys, getopt, time
 
-def arguments(argv, difficulty=None, data="COMSM0010cloud"):
-  try: opts, args = getopt.getopt(argv[1:], "hd:a:", ["difficulty=","data="])
+def arguments(argv, num_of_workers=1, difficulty=None, data="COMSM0010cloud"):
+  try:
+    opts, args = getopt.getopt(
+      argv[1:],
+      "hd:a:n:",
+      ["difficulty=","data=", "num_of_workers="]
+    )
   except getopt.GetoptError:
-    print('simple_finder.py -d <difficulty> -a <data>')
+    print('cnd.py -n <num_of_workers> -d <difficulty> -a <data>')
     sys.exit(2)
   for opt, arg in opts:
     if opt == '-h':
-      print('simple_finder.py -d <difficulty> -a <data>')
+      print('cnd.py -n <num_of_workers> -d <difficulty> -a <data>')
       sys.exit()
+    elif opt in ("-n", "--num_of_workers"): num_of_workers = int(arg) or num_of_workers
     elif opt in ("-d", "--difficulty"): difficulty = int(arg)
-    elif opt in ("-a", "--data"): data = arg
-  return (difficulty, data)
+    elif opt in ("-a", "--data"): data = arg or data
+  if(not difficulty or not data or not num_of_workers):
+    raise ValueError("N and Data are mandatory")
+  return (num_of_workers, difficulty, data)
