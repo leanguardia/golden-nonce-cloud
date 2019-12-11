2bdb8c50829fda67400dd376d5191e616d626f7b
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-12-11 15:07:25 +0000

    Worker infinitely poll tasks

--- worker.py
+++ worker.py
@@ -1,6 +1,6 @@
 import datetime
 from lib.sqs import Sqs
-from lib.tasks_queue import TasksQueue
+from remote.tasks_queue import TasksQueue
 from app.nonce_evaluator import NonceEvaluator
 import os
 
@@ -14,31 +14,29 @@ class Worker(object):
     max_sequence_length = 32
 
     while(searching):
-      tasks = self.tasks_queue.poll_tasks(max_attempts=5)
-      if tasks:
-        while(searching and tasks):
-          task = tasks[0]
-          nonce, search_to, difficulty, data = self.unwrap_task(task)
-          print(task["Body"])
+      tasks = self.tasks_queue.poll_tasks(max_attempts=15)
+      while(searching and tasks):
+        task = tasks[0]
+        nonce, search_to, difficulty, data = self.unwrap_task(task)
+        print(task["Body"])
+        
+        while(searching and nonce <= search_to):
+          binary_sequence = bin(nonce)[2:]
           
-          while(searching and nonce <= search_to):
-            binary_sequence = bin(nonce)[2:]
-            
-            while (searching and len(binary_sequence) <= max_sequence_length):
-              evaluator = NonceEvaluator(data, binary_sequence, difficulty)
-              if (evaluator.valid_nonce()):
-                sqs.send_golden_nonce(nonce, binary_sequence, evaluator.hexdigest)
-                searching = False
-              binary_sequence = "0" + binary_sequence
-            nonce += 1
+          while (searching and len(binary_sequence) <= max_sequence_length):
+            evaluator = NonceEvaluator(data, binary_sequence, difficulty)
+            if (evaluator.valid_nonce()):
+              print("Golden Nonce:", nonce, "|", binary_sequence +"("+ str(len(binary_sequence)) +")")
+              sqs.send_golden_nonce(nonce, binary_sequence, evaluator.hexdigest)
+              searching = False
+            binary_sequence = "0" + binary_sequence
+          nonce += 1
 
-          self.tasks_queue.delete_message(task["ReceiptHandle"])
-          stop_message = sqs.stop_search(max_retries=1)
-          if stop_message != False: searching = False
-          tasks = tasks[1:]
-      else:
-        print("No tasks to fulfill")
-        searching = False
+        self.tasks_queue.delete_message(task["ReceiptHandle"])
+        stop_message = sqs.stop_search(max_retries=1)
+        if stop_message: searching = False
+        tasks = tasks[1:]
+    
     # report what I've done
     print("I'm leaving, no purpose in life!")
 
