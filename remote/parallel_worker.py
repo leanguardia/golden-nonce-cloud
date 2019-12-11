87c8272fa5b4c7561fc570394f52222a585f9b34
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-12-11 01:10:55 +0000

    ParallelWorker wait until instance is running

--- remote/parallel_worker.py
+++ remote/parallel_worker.py
@@ -1,4 +1,4 @@
-import boto3
+import boto3, time
 
 class ParallelWorker(object):
   def __init__(self, num_of_workers):
@@ -7,7 +7,7 @@ class ParallelWorker(object):
     self.instance_ids = []
 
   def run(self):
-    print(f"Initializing {self.num_of_workers} workers")
+    print(f"Initializing {self.num_of_workers} worker(s)")
     response = self.ec2.run_instances(
       LaunchTemplate = {'LaunchTemplateId': 'lt-0244253c92f5a11c0'},
       MinCount = self.num_of_workers,
@@ -15,25 +15,46 @@ class ParallelWorker(object):
     )
     for machine_info in response["Instances"]:
       self.instance_ids.append(machine_info['InstanceId'])
+    print(self.instance_ids)
     return self.instance_ids
 
   def status(self):
     reservations = self.ec2.describe_instances(
-      Filters = [
-        { 'Name': 'tag:Purpose', 'Values': ['Worker'] },
-        { 'Name': 'instance-state-name', 'Values': ['running'] },
-      ],
+      # Filters = [
+      #   { 'Name': 'tag:Purpose', 'Values': ['Worker'] },
+      #   { 'Name': 'instance-state-name', 'Values': ['running'] },
+      # ],
+      InstanceIds=self.instance_ids,
     )['Reservations']
-    return reservations[0]['Instances'] if reservations else []
+    if reservations[0]: statuses = self.__parse_statuses(reservations)
+    return statuses if statuses else []
+
+  def wait_initialization(self, min_instances_running=1, timeout=600):
+    print(f"Waiting until at least {min_instances_running} worker(s) starts running.")
+    waiting = True; seconds = 0
+    while(waiting and seconds < timeout):
+      time.sleep(1); seconds +=1
+      if seconds > 12 and any(state == 'running' for state in self.status()):
+        waiting = False
+      print('.', end='', flush=True)
+    print('')
+    if seconds == timeout: print('Initialization timeout!')
 
   def shutdown(self):
     print(f"Shutting-down instances {self.instance_ids}")
     return self.ec2.terminate_instances(InstanceIds=self.instance_ids)
 
+  def __parse_statuses(self, reservations):
+    statuses = []
+    if reservations[0]:
+      for instance_info in reservations[0]['Instances']:
+        statuses.append(instance_info['State']['Name'])
+    return statuses
+
 if __name__ == '__main__':
 
-  parallel_worker = ParallelWorker(num_of_workers=3)
+  parallel_worker = ParallelWorker(num_of_workers=1)
   parallel_worker.run()
-  statuses = parallel_worker.status()
-  print(statuses)
+  parallel_worker.wait_initialization()
+  print("--> Running")
   # parallel_worker.shutdown()
