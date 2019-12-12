637eca84721b431e1e668e904f43f2fd5b2c68c5
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-12-12 02:19:21 +0000

    ParallelWorker decouple account specific data

--- remote/parallel_worker.py
+++ remote/parallel_worker.py
@@ -1,17 +1,26 @@
-import boto3, time
+import boto3, time, os
 
 class ParallelWorker(object):
   def __init__(self, num_of_workers):
     self.num_of_workers = num_of_workers
     self.ec2 = boto3.client('ec2')
+    self.key_name = os.environ.get('CND_KEY_NAME')
+    self.security_group = os.environ.get('CND_SECURITY_GROUP')
+    self.iam_profile = os.environ.get('CND_IAM_PROFILE')
     self.instance_ids = []
 
   def run(self):
     print(f"Initializing {self.num_of_workers} worker(s)")
+    print("KeyName:", self.key_name, " - SecurityGroup:", self.security_group, " - IAM Profile:", self.iam_profile)
     response = self.ec2.run_instances(
-      LaunchTemplate = {'LaunchTemplateId': 'lt-0244253c92f5a11c0'},
+      ImageId = 'ami-015dd0c2828bf03ff',
+      InstanceType = 't2.micro',
       MinCount = self.num_of_workers,
       MaxCount = self.num_of_workers,
+      UserData = open("remote/worker-boot.txt").read(),
+      KeyName = self.key_name,
+      SecurityGroups = [self.security_group],
+      IamInstanceProfile = { 'Name': self.iam_profile },
     )
     for machine_info in response["Instances"]:
       self.instance_ids.append(machine_info['InstanceId'])
@@ -20,10 +29,6 @@ class ParallelWorker(object):
 
   def status(self):
     reservations = self.ec2.describe_instances(
-      # Filters = [
-      #   { 'Name': 'tag:Purpose', 'Values': ['Worker'] },
-      #   { 'Name': 'instance-state-name', 'Values': ['running'] },
-      # ],
       InstanceIds=self.instance_ids,
     )['Reservations']
     if reservations[0]: statuses = self.__parse_statuses(reservations)
@@ -34,7 +39,7 @@ class ParallelWorker(object):
     waiting = True; seconds = 0
     while(waiting and seconds < timeout):
       time.sleep(1); seconds +=1
-      if seconds > 12 and any(state == 'running' for state in self.status()):
+      if seconds > 12 and all(state == 'running' for state in self.status()):
         waiting = False
       print('.', end='', flush=True)
     print('')
@@ -55,6 +60,5 @@ if __name__ == '__main__':
 
   parallel_worker = ParallelWorker(num_of_workers=1)
   parallel_worker.run()
-  parallel_worker.wait_initialization()
-  print("--> Running")
+  # parallel_worker.wait_initialization()
   # parallel_worker.shutdown()
