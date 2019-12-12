e73438d128e64921da7060f303aab371676bf5f6
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-12-12 18:32:31 +0000

    Plot performance Cloud by specific difficulty

--- out/plotter.py
+++ out/plotter.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 def simple_finder_performance():
@@ -17,6 +18,17 @@ def simple_finder_performance():
   plt.legend()
   plt.savefig("out/performance-local-finder.png")
 
+def cloud_finder_performance(difficulty):
+  data = pd.read_csv('out/cloud-finder.csv', delim_whitespace=True)
+  is_difficulty = data[data['difficulty'] == difficulty]
+  mean_time_by_n_workers = is_difficulty.groupby(['n_workers']).mean()
+  plt.plot(range(1, 8), mean_time_by_n_workers['seconds'])
+  plt.xlabel('Number of workers')
+  plt.ylabel('Seconds')
+  plt.title(f"Difficulty {difficulty}")
+  plt.savefig(f"out/perf-cloud-diff-{difficulty}.png")
+
 if __name__ == "__main__":
-  simple_finder_performance()
-  # cloud_finder_performance()
+  # simple_finder_performance()
+  # cloud_finder_performance(6)
+  cloud_finder_performance(7)
