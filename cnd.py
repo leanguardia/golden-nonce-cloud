606d520872099dd9a35b1952182a897e2d3164e9
D. Leandro Guardia V <lean.guardia93@gmail.com>
2019-12-13 01:34:48 +0000

    Tidy up CND

--- cnd.py
+++ cnd.py
@@ -11,54 +11,48 @@ class Cnd(object):
     self.difficulty = difficulty
     self.tasks_queue = TasksQueue()
     self.stop_queue = StopQueue()
-    
     self.batch_index = 0
-    self.num_of_tests = 25000
+    self.tests_per_task = 25000
 
-  def direct_specification(self, num_of_vms, init_tasks, tasks_per_batch):
+  def direct_specification(self, num_of_vms):
     self.report_search(num_of_vms)
     parallel_worker = ParallelWorker(num_of_workers = num_of_vms)
     parallel_worker.run()
-
     self.tasks_queue.purge()
     self.stop_queue.purge()
+    min_available_tasks = num_of_vms * 3
 
-    min_available_tasks = 80
-
-    num_of_batches = int(init_tasks / 10)
-    self.send_batches(num_of_batches)
-
-    num_of_batches = int(tasks_per_batch / 10)
-    searching = True
-
-    decision_index = 0
+    self.send_batches(min_available_tasks + 1)
     parallel_worker.wait_initialization()
 
     start_time = time.time()
+    self.search_loop(num_of_vms, min_available_tasks)
+    processing_time = time.time() - start_time
+
+    print("Processing time: {0:.3f} seconds".format(processing_time))
+    parallel_worker.shutdown()
+    self.report_stop(self.poll_stop_message())
+  
+  def search_loop(self, num_of_vms, min_available_tasks):
+    searching = True
+    decision_index = 0
     while searching:
       if decision_index % 25 == 0:
         decision_index = 0
         available_tasks_count = self.tasks_queue.approx_num_of_tasks()
         print("Available tasks:", available_tasks_count)
-        if available_tasks_count < min_available_tasks:
-          self.send_batches(num_of_batches)
+        if available_tasks_count <= min_available_tasks:
+          self.send_batches(min(num_of_vms, 10))
       if self.stop_queue.stop(): searching = False
       decision_index +=1 
-
-    processing_time = time.time() - start_time
-    print("Processing time: {0:.3f} seconds".format(processing_time))
-
-    parallel_worker.shutdown()
-
-    self.report_stop(self.poll_stop_message())
 
   def send_batches(self, num_of_batches):
     for index in range(num_of_batches):
       self.send_batch()
 
   def send_batch(self):
-    print(f"Batch {self.batch_index} | 10 tasks of size {self.num_of_tests} each.")
-    response = self.tasks_queue.send_ten_tasks(self.data, self.difficulty, self.batch_index, self.num_of_tests)
+    print(f"Batch {self.batch_index} | 10 tasks of size {self.tests_per_task} each.")
+    response = self.tasks_queue.send_ten_tasks(self.data, self.difficulty, self.batch_index, self.tests_per_task)
     self.batch_index += 1
   
   def poll_stop_message(self):
@@ -75,8 +69,6 @@ class Cnd(object):
       self.report_golden_nonce(stop_message)
     elif stop_reason == "EmergencyScram":
       print("Call 999!")
-
-    print("Sending Report ..")
 
   def report_search(self, num_of_vms):
     print("Direct Specification Search:")
@@ -95,9 +87,4 @@ if __name__ == "__main__":
   n, difficulty, data = util.arguments(sys.argv)
 
   cnd = Cnd(data, difficulty)
-  # cnd.send_batches()
-  cnd.direct_specification(
-    num_of_vms = n,
-    init_tasks = 120,
-    tasks_per_batch = 20,
-  )
+  cnd.direct_specification(num_of_vms = n)
